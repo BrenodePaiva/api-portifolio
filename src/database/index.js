1c3414b4225351cb2543c3bdedbda1d490686156
BrenodePaiva <brenopaiva552@gmail.com>
2026-05-06 14:42:34 -0300

    fix: resete pass user 2.1

--- src/database/index.js
+++ src/database/index.js
@@ -19,6 +19,8 @@ class DataBase {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models))
+
+      console.log('Atributos do User:', Object.keys(User.rawAttributes))
   }
 }
 
